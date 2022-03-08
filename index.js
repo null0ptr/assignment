0c8964f196d826e3087c36c38af9c7620061d638
nazmuShakib <nazshak6@gmail.com>
2022-03-08 15:35:11 +0600

    create index file

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const PORT = (process.env.PORT || 3000)
+app.use(express.urlencoded({
+	extended: "false"
+}))
+app.use(express.json())
+
+app.all('/', (req, res) => {
+	res.send('Index Page')
+})
+app.listen(PORT, () => {
+	console.log(`Server running on PORT ${PORT}`)
+})
