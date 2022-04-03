f977d3e7c578f8d3918fafeb45f20bb9703ae090
nazmuShakib <nazshak6@gmail.com>
2022-04-04 01:48:18 +0600

    add feature for leader endpoint to interact with mongoDB

--- Routers/leaderRouter.js
+++ Routers/leaderRouter.js
@@ -1,22 +1,74 @@
 const express = require('express')
 const router = express.Router()
+const Leaders = require('./Schemas/leaders')
 
-router.route('/')
-	.all((req, res) => {
-		res.send(`${req.method} request on Leader router`)
+
+const getLeader = async (req, res) => {
+	const leader = await Leaders.find({})
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(leader)
+}
+const getPromotionById = async (req, res) => {
+	const leader = await Leaders.findById(req.params.promoId)
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(leader)
+}
+const createPromotion = async (req, res) => {
+	const leader = await Leaders.create(req.body)
+	console.log(leader)
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(leader)
+}
+const deletePromotion = async (req, res) => {
+	const response = await Leaders.deleteMany({})
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(response)
+}
+const updatePromotionById = async (req, res) => {
+	const leader = await Leaders.findByIdAndUpdate(req.params.promoId, {
+		$set: req.body
+	}, {
+		new: true
 	})
-router.route('/:leaderId')
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(leader)
+}
+const deletePromotionById = async (req, res) => {
+	const response = Leaders.findByIdAndDelete(req.params.promoId)
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(response)
+}
+router.route('/')
 	.get((req, res) => {
-		res.send(`GET request on Leader Id = ${req.params.leaderId}`)
+		getLeader(req, res)
 	})
 	.post((req, res) => {
-		res.send(`POST request on Leader Id = ${req.params.leaderId}`)
+		createPromotion(req, res)
 	})
 	.put((req, res) => {
-		res.send(`PUT request on Leader Id = ${req.params.leaderId}`)
+		res.status(403).send('PUT operation not allowed')
 	})
 	.delete((req, res) => {
-		res.send(`DELETE request on Leader Id = ${req.params.leaderId}`)
+		deletePromotion(req, res)
+	})
+router.route('/:promoId')
+	.get((req, res) => {
+		getPromotionById(req, res)
+	})
+	.post((req, res) => {
+		res.status(403).send('POST operation not allowed')
+	})
+	.put((req, res) => {
+		updatePromotionById(req, res)
+	})
+	.delete((req, res) => {
+		deletePromotionById(req, res)
 	})
 	.all((req, res) => {
 		res.send('Invalid Request')
