c29e34401f731b8c841f732b9ff3fbc5beae8d92
nazmuShakib <nazshak6@gmail.com>
2022-04-06 23:49:57 +0600

    add feature for dish endpoint to interact with mongoDB

--- Routers/dishRouter.js
+++ Routers/dishRouter.js
@@ -1,22 +1,73 @@
 const express = require('express')
 const router = express.Router()
+const Dishes = require('./Schemas/dishes')
 
-router.route('/')
-	.all((req, res) => {
-		res.send(`${req.method} request on dishes router`)
+const getDish = async (req, res) => {
+	const Dish = await Dishes.find({})
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(Dish)
+}
+const getDishById = async (req, res) => {
+	const Dish = await Dishes.findById(req.params.promoId)
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(Dish)
+}
+const createDish = async (req, res) => {
+	const Dish = await Dishes.create(req.body)
+	console.log(Dish)
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(Dish)
+}
+const deleteDish = async (req, res) => {
+	const response = await Dishes.deleteMany({})
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(response)
+}
+const updateDishById = async (req, res) => {
+	const Dish = await Dishes.findByIdAndUpdate(req.params.promoId, {
+		$set: req.body
+	}, {
+		new: true
 	})
-router.route('/:dishId')
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(Dish)
+}
+const deleteDishById = async (req, res) => {
+	const response = Dishes.findByIdAndDelete(req.params.promoId)
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/json')
+	res.json(response)
+}
+router.route('/')
 	.get((req, res) => {
-		res.send(`GET request on Dish Id = ${req.params.dishId}`)
+		getDish(req, res)
 	})
 	.post((req, res) => {
-		res.send(`POST request on Dish Id = ${req.params.dishId}`)
+		createDish(req, res)
 	})
 	.put((req, res) => {
-		res.send(`PUT request on Dish Id = ${req.params.dishId}`)
+		res.status(403).send('PUT operation not allowed')
 	})
 	.delete((req, res) => {
-		res.send(`DELETE request on Dish Id = ${req.params.dishId}`)
+		deleteDish(req, res)
+	})
+router.route('/:promoId')
+	.get((req, res) => {
+		getDishById(req, res)
+	})
+	.post((req, res) => {
+		res.status(403).send('POST operation not allowed')
+	})
+	.put((req, res) => {
+		updateDishById(req, res)
+	})
+	.delete((req, res) => {
+		deleteDishById(req, res)
 	})
 	.all((req, res) => {
 		res.send('Invalid Request')
